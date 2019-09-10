a08c43837171311d039976d2bbc8108fdeb8c3a5
Iran Ribeiro <ifreitasribeiro@hotmail.com>
2019-09-10 11:11:03 -0300

    Controle de posicionamento funcionando

--- formation_contr.py
+++ formation_contr.py
@@ -34,13 +34,14 @@ class Controller:
     def initSubscribers(self):
         # --> metodo que vai atualizar dados da mensagem para atributos do objeto
         #self.subvel = self.rospy.Subscriber("/turtle1/pose", Pose, self.get_robot_velocity)
-        #self.subOdom = self.rospy.Subscriber('/%s/odom'%(self.bebop1_name), Odometry, self.get_drone_odom)
-        self.subTurtle = self.rospy.Subscriber('/%s/pose'%("turtle1"), Pose, self.get_turtle_odom)
+        self.subOdom = self.rospy.Subscriber('/%s/odom'%(self.bebop1_name), Odometry, self.get_drone_odom)
+        #self.subTurtle = self.rospy.Subscriber('/%s/pose'%("turtle1"), Pose, self.get_turtle_odom)
         return
     def initPublishers(self, bebop1_name):
-        #self.pubVel = rospy.Publisher('/%s/cmd_vel'%(bebop1_name), Twist, queue_size=10)
+        self.pubVel = rospy.Publisher('/%s/cmd_vel'%(bebop1_name), Twist, queue_size=10)
+        self.pubLand = rospy.Publisher('/%s/land'%(bebop1_name), Empty, queue_size=10)
 
-        self.pubVel2 = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
+        #self.pubVel2 = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
 
         return 
     def initTimeArray(self, start_time, end_time):
@@ -48,58 +49,60 @@ class Controller:
 
     def initvariables(self):
         self.bebop1_name = 'bebop1'
-        self.rate = self.rospy.Rate(200)
-        #self.Xd = np.transpose([[2, 1, 1.5, 0]])   
-        self.Xd = np.transpose([[8, 5.54, 0, 0]])
+        self.rate = self.rospy.Rate(10)
+        self.Xd = np.transpose([[2, 1, 1.5, 0]])   
+        #self.Xd = np.transpose([[8, 5.54, 0, 0]])
         
         self.ti = 0.1
-        self.tfinal = 10
+        self.tfinal = 20
         self.t = np.arange(0, self.tfinal, self.ti)
 
         #Formação desejada
-        #self.qdes = np.transpose([[2, 1, 1.5, 1, 0, 0]])
-        self.qdes = np.transpose([[8, 5.54, 0, 1, 0, 0]])
+        self.qdes = np.transpose([[2, 1, 1.5, 1, 0, 0]])
+        #self.qdes = np.transpose([[8, 5.54, 0, 1, 0, 0]])
         
         self.qtil = np.zeros((6, len(self.t)))
 
         #ks
-        self.k1 = 5
-        self.k2 = 0.3
-        self.k3 = 5.5
-        self.k4 = 0.2
-        self.k5 = 0.55
-        self.k6 = 0.7
-        self.k7 = 1.45
-        self.k8 = 0.7        
+        self.k1 = 4.3321
+        self.k2 = 0.2333
+        self.k3 = 1.1161
+        self.k4 = 0.3786
+        self.k5 = 4.2492
+        self.k6 = 4.3235
+        self.k7 = 10.4174
+        self.k8 = 5.9794    
 
         #kps
-        self.kpx = 3
-        self.kpy = 3
-        self.kpz = 0.5
-        self.kpphi = 3
-        self.kdx = 2
-        self.kdy = 2.5
-        self.kdz = 2
-        self.kdphi = 0.5
+        self.kpx = 1
+        self.kpy = 0.5
+        self.kpz = 0.7
+        self.kpphi = 0.7
+        self.kdx = 0.5
+        self.kdy = 1
+        self.kdz = 0.8
+        self.kdphi = 0.7
         #kp
         self.kp = np.diag([self.kpx, self.kpy, self.kpz, self.kpphi])
         self.kd = np.diag([self.kdx, self.kdy, self.kdz, self.kdphi])
 
         #Condições iniciais
-        self.x = 5.54*np.ones((len(self.t), 1))
-        self.y = 5.54*np.ones((len(self.t),1))
-        self.z = 0*np.ones((len(self.t),1))
+        self.x = 0*np.ones((len(self.t), 1))
+        self.y = 0*np.ones((len(self.t),1))
+        self.z = np.ones((len(self.t),1))
         self.phi = np.zeros((len(self.t),1))
         self.U = {}
-        self.ganho = 0.5
+        self.ganho = 0.4
 
-        self.odom_drone = (5.54, 5.54, 0, 0)
-        self.old_odom_drone = (0,0,0,0)
+        self.odom_drone = (0, 0, 1, 0)
+        self.old_odom_drone = (0,0,1,0)
 
         self.a=0.2
         self.v = {}
-        self.odomx = 5.54*np.ones(len(self.t)-1)
-        self.odomy = 5.54*np.zeros(len(self.t)-1)
+        self.odomx = 0*np.ones(len(self.t)-1)
+        self.odomy = 0*np.zeros(len(self.t)-1)
+        self.odomz = 1*np.zeros(len(self.t)-1)
+        self.erro = np.zeros((3,len(self.t)-1))
 
 
     def get_robot_velocity(self,msg):
@@ -113,7 +116,7 @@ class Controller:
         x = msg.pose.pose.position.x
         y = msg.pose.pose.position.y
         z = msg.pose.pose.position.z
-        psi = msg.twist.twist.angular.z
+        psi = msg.pose.pose.orientation.z
 
         #atualiza o valor da odometria
         self.odom_drone = (x, y, z, psi)
@@ -136,10 +139,55 @@ class Controller:
             rate_10.sleep()
         return True
 
+    def land(self, drone_name):
+        pub = self.rospy.Publisher("/%s/land"%(drone_name), Empty, queue_size=10)    
+        rate_10 = self.rospy.Rate(10) # 10hz
+        for i in range(1,25):
+            pub.publish(Empty())
+            rate_10.sleep()
+        return True
+
+    def controlePosicionamento(self, vel_msg = None, Xtil=None, j=None, phi = None):
+        f1 = [  
+            [(self.k1)*(math.cos(phi[j])),(-self.k3)*(math.sin(phi[j])), 0, 0],
+            [(self.k1)*math.sin(phi[j]), self.k3*math.cos(phi[j]), 0, 0],
+            [0, 0, self.k5, 0],
+            [0, 0, 0, self.k7]]
+        f1_inv = np.linalg.inv(f1)
+        #print ("f1-1", f1_inv)
+        xd_x_to = self.Xd*self.ti
+        #print ("xd*xto \n", xd_x_to)
+
+        kd_Xtil = self.kd.dot(Xtil)
+
+        #print ("kd_Xtil \n", kd_Xtil)
+
+        tanh_kd_Xtil = np.transpose([np.array([math.tanh(xi) for xi in kd_Xtil])])
+        #print("tanh_kd_Xtil \n",tanh_kd_Xtil)
+                
+        part3 = self.kp.dot(tanh_kd_Xtil)
+
+        #print("kp*tanh_kd_til \n", part3)
+        
+        xd_kp = xd_x_to + part3
+        #print ("part1 \n", f1_inv_x_xd.diagonal()) 
+        #print ("xd_x_to + part3 \n", xd_kp)
+        #f1_inv_x_xd = (f1_inv).dot(xd_kp)
+
+        #print ("f1_inv_x_xd \n", f1_inv_x_xd) 
+        self.U[j] = ((f1_inv).dot(xd_kp))*self.ganho
+
+        return self.U
+
+    def controleFormacao(self, odomx, odomy, odomz):
+        xf = odomx
+        yf = odomy
+        zf = odomz
+
     def run(self):        
 
         #a execução espera o comando de takeoff finalizar
-        #self.takeOff(self.bebop1_name)  
+        self.takeOff(self.bebop1_name)  
         #print(self.t)
         vel_msg = Twist()
 
@@ -147,56 +195,28 @@ class Controller:
             X = np.transpose([[self.odom_drone[0] , self.odom_drone[1], self.odom_drone[2], self.odom_drone[3]]])
             self.odomx[i] = self.odom_drone[0]
             self.odomy[i] = self.odom_drone[1]
+            self.odomz[i] = self.odom_drone[2]
+
             #X = np.transpose([[self.x[i] , self.y[i], self.z[i], self.phi[i]]])
+
             Xtil = self.Xd - X
-            f1 = [
-                [(self.k1)*(math.cos(self.phi[i])),(-self.k3)*(math.sin(self.phi[i])), 0, 0],
-                [(self.k1)*math.sin(self.phi[i]), self.k3*math.cos(self.phi[i]), 0, 0],
-                [0, 0, self.k5, 0],
-                [0, 0, 0, self.k7]]
-            f1_inv = np.linalg.inv(f1)
-            #print ("f1-1", f1_inv)
-            xd_x_to = self.Xd*self.ti
-            #print ("xd*xto \n", xd_x_to)
+            self.erro[0,i] = Xtil[0,0]
+            self.erro[1,i] = Xtil[1,0]
+            self.erro[2,i] = Xtil[2,0]
 
-            kd_Xtil = self.kd.dot(Xtil)
-
-            #print ("kd_Xtil \n", kd_Xtil)
-
-            tanh_kd_Xtil = np.transpose([np.array([math.tanh(xi) for xi in kd_Xtil])])
-            #print("tanh_kd_Xtil \n",tanh_kd_Xtil)
-                    
-            part3 = self.kp.dot(tanh_kd_Xtil)
-
-            #print("kp*tanh_kd_til \n", part3)
+            U = self.controlePosicionamento(vel_msg=vel_msg, Xtil=Xtil, j=i, phi=self.phi)
             
-            xd_kp = xd_x_to + part3
-            #print ("part1 \n", f1_inv_x_xd.diagonal()) 
-            #print ("xd_x_to + part3 \n", xd_kp)
-            #f1_inv_x_xd = (f1_inv).dot(xd_kp)
-
-            #print ("f1_inv_x_xd \n", f1_inv_x_xd) 
-            self.U[i] = ((f1_inv).dot(xd_kp))*self.ganho
-
-            vel_msg.linear.x = self.U[i][0][0]
-            vel_msg.linear.y = self.U[i][1][0]
-            vel_msg.linear.z = self.U[i][2][0]
-            vel_msg.angular.x = 0
-            vel_msg.angular.y = 0
-            vel_msg.angular.z = 0
+            vel_msg.linear.x = U[i][0][0]
+            vel_msg.linear.y = U[i][1][0]
+            vel_msg.linear.z = U[i][2][0]
+            vel_msg.angular.z = U[i][3][0]
 
             if not self.rospy.is_shutdown():                
-                self.pubVel2.publish(vel_msg)
-            time.sleep(self.ti)
-            
-            #print (self.U[i][0][0], self.U[i][1][0], self.U[i][2][0])
+                self.pubVel.publish(vel_msg)
+            self.rate.sleep()
+
+            """
             #Define a nova posicão do drone1
-            self.x[i+1] = self.x[i]+self.ti*self.U[i][0][0]
-            self.y[i+1] = self.y[i]+self.ti*self.U[i][1][0]
-            self.z[i+1] = self.z[i]+self.ti*self.U[i][2][0]
-            self.phi[i+1] = self.phi[i]+self.ti*self.U[i][3][0]
-            
-            #time.sleep(self.ti)
 
             xf = self.x[i][0]
             yf = self.y[i][0]
@@ -214,23 +234,37 @@ class Controller:
 
             
             
-            self.qtil[:,i] = (self.qdes - np.transpose(q))[:,0]       
-            
+            #self.qtil[:,i] = (self.qdes - np.transpose(q))[:,0]       
+            qtil = (self.qdes - np.transpose(q))[:,0]  
 
             #Ganhos
             L1 = 0.2*np.identity(6)
             L2 = 0.3*np.identity(6)            
-            L2_x_qtil = L2.dot(self.qtil[:,i])
+            L2_x_qtil = L2.dot(qtil)
             tanh_L2qtil = [math.tanh(li) for li in L2_x_qtil]         
             qrefp = L1.dot(tanh_L2qtil) 
-            jacob =np.array([[1, 0, 0, 0, 0, 0],
+
+            
+            jacob2 = np.array([
+                [1, 0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0, 0],
+                [1, 0, 0, math.cos(alphaf)*math.cos(betaf), -rhof*math.sin(alphaf)*math.cos(betaf), -rhof*math.cos(alphaf)*math.sin(betaf)],
+                [0, 1, 0, math.sin(alphaf)*math.cos(betaf), rhof*math.cos(alphaf)*math.cos(betaf),  -rhof*math.sin(alphaf)*math.sin(betaf)],
+                [0, 0, 1, math.sin(betaf), 0, rhof*math.cos(betaf)]])
+
+
+
+
+            jacob = np.array([
+                    [1, 0, 0, 0, 0, 0],
                     [0, 1, 0, 0, 0, 0],
                     [0, 0, 1, 0, 0, 0],
                     [1, 0, 0, math.cos(alphaf)*math.cos(betaf), -rhof*math.sin(alphaf)*math.cos(betaf), -rhof*math.cos(alphaf)*math.sin(betaf)],
                     [0, 1, 0, math.sin(alphaf)*math.cos(betaf), -rhof*math.sin(alphaf)*math.sin(betaf),  -rhof*math.cos(alphaf)*math.cos(betaf)],
                     [0, 0, 1, math.sin(betaf), rhof*math.cos(betaf), 0]])
 
-            xrefp = jacob.dot(qrefp)
+            xrefp = jacob2.dot(qrefp)
 
             K = np.array([  [math.cos(self.z[i]), math.sin(self.z[i]), 0, 0, 0, 0],
                             [-math.sin(self.z[i])/self.a, math.cos(self.z[i])/self.a, 0, 0, 0, 0],
@@ -239,7 +273,21 @@ class Controller:
                             [0, 0, 0, math.sin(self.phi[i]), math.cos(self.phi[i]), 0],
                             [0, 0, 0, 0, 0, 1]])
 
-            self.v[i] = K.dot(xrefp)
+            self.v[i] = K.dot(xrefp)"""
+
+
+            """ vel_msg.linear.x = self.v[i][1]
+            vel_msg.linear.y = self.v[i][0]
+            vel_msg.linear.z = self.v[i][2]
+            vel_msg.angular.x = 0
+            vel_msg.angular.y = 0
+            vel_msg.angular.z = self.v[i][3]
+
+
+            if not self.rospy.is_shutdown():                
+                self.pubVel.publish(vel_msg)
+            self.rate.sleep()"""
+
             """
             vel_msg.linear.x = self.v[i][0]
             vel_msg.linear.y = self.v[i][1]
@@ -279,17 +327,21 @@ class Controller:
             Utheta (UVx) = comando de inclinação em relação ao eixo yW (eixo xB do drone)
 
             """
-       
-        #mpl.rcParams['legend.fontsize'] = 10
-        print (self.odomx)
-        fig = plt.figure()
-        ax = fig.gca(projection='3d')
 
-        #ax = Axes3D(plt.gcf())
-        ax.plot(self.odomx, self.odomy, label='Experimento')
-        ax.plot(self.x[:,0],self.y[:,0],label='Simulação')
-        plt.legend()
+        self.land("bebop1")
+        #mpl.rcParams['legend.fontsize'] = 10
+        fig = plt.figure()
         
+        """ax = fig.gca(projection='3d')
+
+        ax = Axes3D(plt.gcf())
+        ax.plot(self.odomx, self.odomy, self.odomz, label='Experimento')
+        ax.plot(self.x[:,0],self.y[:,0], self.z[:,0],label='Simulação')"""
+
+        plt.plot(self.t[:-1], self.erro[0,:], label="Erro X")        
+        plt.plot(self.t[:-1], self.erro[1,:], label="Erro Y")        
+        plt.plot(self.t[:-1], self.erro[2,:], label="Erro Z")
+        plt.legend()      
         plt.show()
         
     
