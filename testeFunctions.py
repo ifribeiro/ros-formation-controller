dbfb1252c399c80ef2450d4619c6f404d4a49348
Iran Ribeiro <ifreitasribeiro@hotmail.com>
2019-09-26 16:06:17 -0300

    Testes de plotagem

--- testeFunctions.py
+++ testeFunctions.py
@@ -1,81 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 import math
-import numpy as np 
+import numpy as np
+import random as rd 
+import matplotlib.pyplot as plt
 
-odomx=1
-odomy=1
-odomz=2 
-odomx2=0.5
-odomy2=0.8
-odomz2=1
-odomphi = 0.0
-a = 0.2
-To = 0.1
-
-qdes = np.transpose([2, 1, 1.5, 1, 0, 0])
-
-def controleFormacao(odomx=None, odomy=None, odomz=None, odomx2=None, odomy2=None, odomz2=None):
-    xf = odomx
-    yf = odomy
-    zf = odomz
-
-    #rhof
-    rhof = math.sqrt((odomx2 - odomx)**2 + (odomy2 - odomy)**2 + (odomz2 - odomz)**2)
-
-    #alphaf
-    alphaf =  math.atan2((odomy2 - odomy), (odomx2-odomx))
-
-    #betaf
-    betaf = math.atan2((odomz2 - odomz), math.sqrt((odomx2 - odomx)**2 + (odomy2 - odomy)**2))
-
-    #q
-    q = np.array([xf, yf, zf, rhof, betaf, alphaf])
-
-    qtil = (qdes - np.transpose(q))
-
-    #Matriz de ganhos
-    L1 = 0.2*np.identity(6)
-    L2 = 0.3*np.identity(6)    
-
-    #qrefp = L1*tanh(L2*qtil)
-
-    #L2*qtil
-    #retorna uma matriz com uma única columa .shape(6,)
-    L2_x_qtil = L2.dot(qtil)
-    
-    #tanh(L2*qtil)
-    tanh_l2_qtil = [math.tanh(x) for x in L2_x_qtil]
-
-    qrefp = L1.dot(tanh_l2_qtil)
+t = [0,1,2,3,4,5,6,7,8,9,10]
+errox = []
+errorhof = []
+for i in range(0,10):
+    errox.append(rd.randint(0,55))
+    errorhof.append(rd.randint(0,55))
 
 
-    jacob = np.array([
-                [1, 0, 0, 0, 0, 0],
-                [0, 1, 0, 0, 0, 0],
-                [0, 0, 1, 0, 0, 0],
-                [1, 0, 0, math.cos(alphaf)*math.cos(betaf), -rhof*math.sin(alphaf)*math.cos(betaf), -rhof*math.cos(alphaf)*math.sin(betaf)],
-                [0, 1, 0, math.sin(alphaf)*math.cos(betaf), rhof*math.cos(alphaf)*math.cos(betaf),  -rhof*math.sin(alphaf)*math.sin(betaf)],
-                [0, 0, 1, math.sin(betaf), 0, rhof*math.cos(betaf)]])
+fig, ax = plt.subplots(2)
+fig.set_figheight(20)
+fig.set_figwidth(20)
+ax[0].plot(t[:-1], errox[:], label="Erro X", )        
+"""ax[0].plot(t[:i], erroy[:], label="Erro Y")        
+ax[0].plot(t[:i], erroz[:], label="Erro Z")"""
+ax[0].set_title("Erros de posicionamento")
 
+ax[1].plot(t[:-1], errorhof[:], '--', label="Erro rhof")        
+"""ax[1].plot(t[:i], errobetaf[:], '--', label="Erro betaf")        
+ax[1].plot(t[:i], erroalphaf[:], '--', label="Erro alphaf")"""
+ax[1].set_title("Erros de formação")
+ax[1].set_xlabel("Tempo de execução (s)")
+ax[1].set_ylabel("Erro (m)")
 
-    xrefp = jacob.dot(qrefp)
+fig.legend()
 
-
-    K = np.array([
-            [math.cos(odomz), math.sin(odomz), 0, 0, 0, 0],
-            [-math.sin(odomz)/a, math.cos(odomz)/a, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0],
-            [0, 0, 0, math.cos(odomphi), -math.sin(odomphi), 0],
-            [0, 0, 0, math.sin(odomphi), math.cos(odomphi), 0],
-            [0, 0, 0, 0, 0, 1]])
-
-    v = K.dot(xrefp)
-
-    x2 = odomx2+To*v[0]
-    y2 = odomy2+To*v[1]
-    z2 = odomz2+To*v[2]
-
-    return (x2,y2,z2)
-
-q = controleFormacao(odomx=1, odomy=1, odomz=2, odomx2=0.5, odomy2=0.8, odomz2=1)
-
-print (q)
+plt.show()
