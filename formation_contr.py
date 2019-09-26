89ef6b59e03bcf3ad252817174b44f7a58b906ea
Iran Ribeiro <ifreitasribeiro@hotmail.com>
2019-09-26 15:34:38 -0300

    Adição do compensador dinâmico

--- formation_contr.py
+++ formation_contr.py
@@ -11,6 +11,7 @@ import matplotlib as mpl
 import message_filters
 import numpy as np
 import rospy
+import time
 import math
 import tf
 
@@ -104,8 +105,8 @@ class Controller:
         self.kd = np.diag([self.kdx, self.kdy, self.kdz, self.kdphi])
 
 
-        self.ku = np.diag([self.k1, self.k3, self.k5, self.k7])
-        self.kv = np.diag([self.k2, self.k4, self.k6, self.k8])
+        self.Ku = np.diag([self.k1, self.k3, self.k5, self.k7])
+        self.Kv = np.diag([self.k2, self.k4, self.k6, self.k8])
         self.K = np.diag([0.8, 0.8, 0.8, 0.8])
 
         #Condições iniciais
@@ -118,7 +119,7 @@ class Controller:
         self.ganho = 0.4
 
         #(x, y, z, psi, vel_x, vel_y, velz)
-        self.odom_drone = (0, 0, 0, 0, 0, 0 )
+        self.odom_drone1 = (0, 0, 0, 0, 0, 0 )
         
         
         self.old_odom_drone = (0,0,1,0,0,0,0)
@@ -151,6 +152,8 @@ class Controller:
         self.y2 = 1*np.ones((len(self.t), 1))
         self.z2 = 0.75*np.ones((len(self.t),1))
         self.phi2 = np.zeros((len(self.t),1))
+        self.Vd1A = np.transpose([0, 0, 0, 0])
+        self.Vd2A = np.transpose([0, 0, 0, 0])
 
     def get_drone_odom(self, msg):
 
@@ -160,7 +163,7 @@ class Controller:
         msg = Mensagem recebida do tópico
         """
         
-        self.old_odom_drone = self.odom_drone
+        self.old_odom_drone = self.odom_drone1
 
         #Posição
         x = msg.pose.pose.position.x
@@ -173,7 +176,7 @@ class Controller:
         zQ = msg.pose.pose.orientation.z
         wQ = msg.pose.pose.orientation.w
 
-        quaterno = (wQ, xQ, yQ, zQ)
+        quaterno = (xQ, yQ, zQ, wQ)
         euler = tf.transformations.euler_from_quaternion(quaterno)
         #Orientação
         psi = euler[2]
@@ -181,12 +184,13 @@ class Controller:
         vel_x = msg.twist.twist.linear.x
         vel_y = msg.twist.twist.linear.y
         vel_z = msg.twist.twist.linear.z
+        velAngZ = msg.twist.twist.angular.z
 
         #atualiza o valor da odometria
-        self.odom_drone = (x, y, z, psi, vel_x, vel_y, vel_z)
+        self.odom_drone1 = (x, y, z, psi, vel_x, vel_y, vel_z, velAngZ)
 
         #verifica se a odometria mudou
-        if(self.old_odom_drone != self.odom_drone):
+        if(self.old_odom_drone != self.odom_drone1):
             self.change = True
         return
 
@@ -208,7 +212,7 @@ class Controller:
         yQ = msg.pose.pose.orientation.y
         zQ = msg.pose.pose.orientation.z
         wQ = msg.pose.pose.orientation.w
-        quaterno = (wQ, xQ, yQ, zQ)
+        quaterno = (xQ, yQ, zQ, wQ)
         euler = tf.transformations.euler_from_quaternion(quaterno)
         #Orientação
         psi = euler[2]
@@ -216,9 +220,10 @@ class Controller:
         vel_x = msg.twist.twist.linear.x
         vel_y = msg.twist.twist.linear.y
         vel_z = msg.twist.twist.linear.z
+        velAngZ = msg.twist.twist.angular.z
 
         #atualiza o valor da odometria
-        self.odom_drone2 = (x, y, z, psi, vel_x, vel_y, vel_z)
+        self.odom_drone2 = (x, y, z, psi, vel_x, vel_y, vel_z, velAngZ)
 
 
     def takeOff(self, drone_name1, drone_name2):
@@ -262,7 +267,7 @@ class Controller:
         self.rospy.loginfo("Feito.")
         return True
 
-    def controleFormacao(self, j=None, odomx=None, odomy=None, odomz=None, odompsi=None, odomx2=None, odomy2=None, odomz2=None, odompsi2=None):
+    def controleFormacao(self, j=None, t=None, odom_drone1=[], odom_drone2=[]):
         """
         Realiza o controle de formação
 
@@ -273,26 +278,32 @@ class Controller:
         Retorna: a velocidade que deve ser aplicada ao segundo drone
 
         """
-        xf = odomx
-        yf = odomy
-        zf = odomz
+
+        odomx = odom_drone1[0]
+        odomy = odom_drone1[1] 
+        odomz = odom_drone1[2]
+        odompsi = odom_drone1[3]
+        vel_x1 = odom_drone1[4]
+        vel_y1 = odom_drone1[5]
+        vel_z1 = odom_drone1[6] 
+        velAngZ1 = odom_drone1[7]
+
+        odomx2 = odom_drone2[0] 
+        odomy2 = odom_drone2[1]
+        odomz2 = odom_drone2[2]
+        odompsi2 = odom_drone2[3]
+        vel_x2 = odom_drone2[4]
+        vel_y2 = odom_drone2[5]
+        vel_z2 = odom_drone2[6] 
+        velAngZ2 = odom_drone2[7]
 
         #rhof
         rhof = math.sqrt((odomx2 - odomx)**2 + (odomy2 - odomy)**2 + (odomz2 - odomz)**2)
-
-        #alphaf
         alphaf =  math.atan2((odomy2 - odomy), (odomx2-odomx))
-
-        #betaf
         betaf = math.atan2((odomz2 - odomz), math.sqrt((odomx2 - odomx)**2 + (odomy2 - odomy)**2))
-
-        #q
-        q = np.array([xf, yf, zf, rhof, alphaf, betaf])
-
-        #qtil
+        q = np.array([odomx, odomy, odomz, rhof, alphaf, betaf])
         qtil = self.qdes - np.transpose(q)
 
-        
         self.errox.append(qtil[0,])
         self.erroy.append(qtil[1,])
         self.erroz.append(qtil[2,])
@@ -327,20 +338,45 @@ class Controller:
 
 
         xrefp = jacob.dot(qrefp)
-
-
-        K = np.array([
-            [math.cos(odompsi), math.sin(odompsi), 0, 0, 0, 0],
-            [-math.sin(odompsi), math.cos(odompsi), 0, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0],
-            [0, 0, 0, math.cos(odompsi2), -math.sin(odompsi2), 0],
-            [0, 0, 0, math.sin(odompsi2), math.cos(odompsi2), 0],
-            [0, 0, 0, 0, 0, 1]])
-
         
-        v = K.dot(xrefp)
+        F1 = np.array([
+            [math.cos(odompsi), -math.sin(odompsi), 0, 0],
+            [math.sin(odompsi), math.cos(odompsi), 0, 0],
+            [0,     0,      1,      0],
+            [0,     0,      0,      1]])
 
-        return v
+        F2 = np.array([
+            [math.cos(odompsi2), -math.sin(odompsi2), 0, 0],
+            [math.sin(odompsi2), math.cos(odompsi2), 0, 0],
+            [0,     0,      1,      0],
+            [0,     0,      0,      1]])
+
+        F1 = np.linalg.inv(F1)
+        F2 = np.linalg.inv(F2)
+
+        Xd1p = np.append(xrefp[:3,], 0)
+        Xd2p = np.append(xrefp[3:,], 0)
+
+        Vd1 = F1.dot(Xd1p)
+        Vd2 = F2.dot(Xd2p)
+
+        #Deriva a velocidade
+        Vd1p = (Vd1 - self.Vd1A)/(self.rospy.get_time()-t)
+        Vd2p = (Vd2 - self.Vd2A)/(self.rospy.get_time()-t)
+
+        #Atualiza as velocidades anteriores
+        self.Vd1A = Vd1
+        self.Vd2A = Vd2
+
+        vel_bp1 = F1.dot(np.transpose([vel_x1,vel_y1,vel_z1,0]))
+        vel_bp2 = F2.dot(np.transpose([vel_x2,vel_y2,vel_z2,0]))
+
+        Ku_inv = np.linalg.inv(self.Ku)
+        Ud1 = (Ku_inv.dot((Vd1p+self.K.dot(Vd1-vel_bp1) + self.Kv.dot(vel_bp1))))
+        Ud2 = (Ku_inv.dot((Vd2p+self.K.dot(Vd2-vel_bp2) + self.Kv.dot(vel_bp2))))
+
+        return (Ud1, Ud2)
+
     def run(self):        
 
         #a execução espera o comando de takeoff finalizar
@@ -357,27 +393,27 @@ class Controller:
         T_CONTROL = 0.2
 
         t = self.rospy.get_time()
+        t_incB = self.rospy.get_time()
         t_control = self.rospy.get_time()
         i = 0
         self.rospy.wait_for_message('/%s/new_odom'%(self.bebop1_name), Odometry)
         
         while ((self.rospy.get_time()-t) < T_MAX):
             if ((self.rospy.get_time() - t_control) > T_CONTROL):            
-                U = self.controleFormacao(j=i, odomx=self.odom_drone[0],odomy=self.odom_drone[1],odomz=self.odom_drone[2],odompsi=self.odom_drone[3],
-                odomx2=self.odom_drone2[0], odomy2=self.odom_drone2[1],odomz2=self.odom_drone2[2], odompsi2 = self.odom_drone2[3])
-                print (U.shape)
-                vel_msg.linear.x = U[0]
-                vel_msg.linear.y = U[1]
-                vel_msg.linear.z = U[2]
+                U = self.controleFormacao(j=i, t=t_incB, odom_drone1=self.odom_drone1, odom_drone2=self.odom_drone2)                
+                vel_msg.linear.x = U[0][0]
+                vel_msg.linear.y = U[0][1]
+                vel_msg.linear.z = U[0][2]
 
-                vel_msg2.linear.x = U[3]
-                vel_msg2.linear.y = U[4]
-                vel_msg2.linear.z = U[5]
+                vel_msg2.linear.x = U[1][0]
+                vel_msg2.linear.y = U[1][1]
+                vel_msg2.linear.z = U[1][2]
 
                 if not self.rospy.is_shutdown():                
                     self.pubVel.publish(vel_msg)
                     self.pubVel2.publish(vel_msg2)
-                t_control = self.rospy.get_time()                
+                t_control = self.rospy.get_time()
+                t_incB = self.rospy.get_time()                
                 i = i + 1
         
         #mpl.rcParams['legend.fontsize'] = 10
